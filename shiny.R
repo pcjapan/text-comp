76d96612dce70769e0bccabd7409af571d3d8252
pcjapan <54536915+pcjapan@users.noreply.github.com>
2023-06-11 19:21:47 +0900

    Setting up new layout

--- shiny.R
+++ shiny.R
@@ -5,73 +5,81 @@ library(readtext)
 library(DT)
 library(bslib)
 
+## Set up the main body layout
+
+cards <- list(
+  card(
+    card_header(
+      h1("Word Count")
+    ),
+      p("Total Words:", textOutput("word_count", inline = T)),
+      p("Unmatched Words:", textOutput("unmatched_word_count", inline = T)),
+      p("Word Coverage (percent):", textOutput("percent_coverage", inline = T)),
+      p("Unique Word Count:", textOutput("unique_word_count", inline = T))
+  ),
+  card(
+    card_header(
+      h1("Annotated Text")
+    ),
+      htmlOutput("text1")
+  ),
+  card(
+    card_header(
+      h1("Off-list Words")
+      ),
+    dataTableOutput("word_counts", fill = FALSE)
+  )
+)
+
+
+
 # Define UI for application
-ui <- fluidPage(
+ui <- page_sidebar(
   theme = bs_theme(
     version = 5,
     bootswatch = "sandstone"),
-  titlePanel("Text File Comparison"),
-  sidebarLayout(
-    sidebarPanel(
+  title = "Text File Comparison",
+  sidebar = sidebar(
+    width = "25%",
       fileInput("file1", "Choose Text File",
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".txt")
-                ),
+      ),
       fileInput("file2", "Choose Word List File",
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".txt")
-                ),
+      ),
       actionButton("process_btn", "Process Text"),
       hr(),
-      card(
-        card_header(
-          h1("Instructions")
-        ),
-        card_body(
-          p("This application was created for a particular need case: To check texts against a user-defined word list so as to receive an indication of the coverage the word list provides. Basic readility statistics are also calculated and reported. To use the application, please follow these steps:"),
-         tags$ul(
-           tags$li("Prepare and save your", tags$b(" text "), "as a plain text file with the ",  tags$i(".txt "),  "prefix"),
-           tags$li("Prepare and save your", tags$b(" wordlist "), "as a plain text file, with each word separated by a comma or by a line break. Save this file as a ",  tags$i(".txt " ), "or ",  tags$i(".csv "),  "file"),
-           tags$li("Make sure there is a", tags$u("line break / carriage return at the end"), "of each document."),
-           tags$li("Use the ", tags$i("Browse..."),  "buttons to find and upload the saved texts on your computer. Click ", tags$i("Process Text"), "when done." )
-           )
+
+        accordion(
+          open = FALSE,
+          accordion_panel(
+            "Click for instructions",
+            card(
+            h1("Instructions"),
+          tags$ul(
+            tags$li("Prepare and save your", tags$b(" text "), "as a plain text file with the ",  tags$i(".txt "),  "prefix"),
+            tags$li("Prepare and save your", tags$b(" wordlist "), "as a plain text file, with each word separated by a comma or by a line break. Save this file as a ",  tags$i(".txt " ), "or ",  tags$i(".csv "),  "file"),
+            tags$li("Make sure there is a", tags$u("line break / carriage return at the end"), "of each document."),
+            tags$li("Use the ", tags$i("Browse..."),  "buttons to find and upload the saved texts on your computer. Click ", tags$i("Process Text"), "when done." ),
+            tags$li("Results will be displayed in the", tags$i("Output"), "panel." )
+          )
+          )
         )
       )
     ),
     mainPanel(
+      h1("Output"),
       conditionalPanel(
         condition = "input.process_btn != 0",
-      card(
-        card_header(
-          h1("Word Count")
-          ),
-        card_body(
-          p("Total Words:", textOutput("word_count", inline = T)),
-          p("Unmatched Words:", textOutput("unmatched_word_count", inline = T)),
-          p("Word Coverage (percent):", textOutput("percent_coverage", inline = T)),
-          p("Unique Word Count:", textOutput("unique_word_count", inline = T))
-          )
-        ),
-     card(
-       card_header(
-         h1("Annotated Text")
-         ),
-       card_body(
-         htmlOutput("text1")
-         )
-      ),
-      card(
-        card_header(
-          h1("Off-list Words")
-          ),
-       card_body(
-         dataTableOutput("word_counts")))
+       !!!cards
       )
     )
   )
-)
+
 
 # Define server logic
 server <- function(input, output) {
@@ -112,6 +120,7 @@ server <- function(input, output) {
     # Create a table of the frequency count of each unmatched word
     word_counts <- data.frame(table(unmatched_table_list))
     colnames(word_counts) <- c("Word", "Count")
+
 
     # Highlight unmatched words in the text
     highlighted_text <- text
